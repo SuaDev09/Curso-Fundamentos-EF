69f91098c4e71483fa8decf19c83e8f8c471e915
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-10 21:58:29 -0700

    first commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectoef.Models;
+
+public class Categoria
+{
+    [Key]
+    [MaxLength(150)]
+    public Guid CategoriaId { get; set; }
+
+    [Required]
+    public string Nombre { get; set; }
+    
+    public string Descripcion { get; set;}
+    
+    public virtual ICollection<Tarea> Tareas { get; set; }
+}
