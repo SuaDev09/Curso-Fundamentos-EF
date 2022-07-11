fcfa66ad13d81d495f7b67d5bbeff3307523f9c1
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-11 14:14:59 -0700

    Se agrego la columna Peso en Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -12,6 +12,9 @@ public class Categoria
     public string Nombre { get; set; }
     
     public string Descripcion { get; set;}
+
+    //Fuerza o impacto de la tarea
+    public int Peso { get; set;}
     
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
