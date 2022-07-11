7b988a978814c23425b39be46deacf1b452894eb
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-10 23:28:05 -0700

    se anadio la cadena de conexion al appsettings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+/*
+Base de datos en memoria
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+*/
+
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
