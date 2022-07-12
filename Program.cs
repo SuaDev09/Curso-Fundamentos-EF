bbe2b76c10378824f73b1b412e72865b896b64cc
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-12 14:39:50 -0700

    creacion de CRUD

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Runtime.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +24,47 @@ app.MapGet("/dbconexion", async([FromServices] TareasContext dbContext) =>
 });
 
 app.MapGet("/api/tareas", async([FromServices] TareasContext dbContext) =>{
-    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == proyectoef.Models.Prioridad.Media));
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));//.Where(p => p.PrioridadTarea == proyectoef.Models.Prioridad.Media));
+});
+
+app.MapPost("/api/tareas", async([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/tareas/{id}", async([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    } 
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/api/task/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) => {
+    var tarea = dbContext.Tareas.Find(id);
+
+    if(tarea == null)
+        return Results.NotFound("Tarea no encontrada.");
+        
+    dbContext.Remove<proyectoef.Models.Tarea>(tarea);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok("Removed!");
 });
 
 app.Run();
